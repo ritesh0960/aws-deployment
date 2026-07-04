dbafaff5e78e42965c1d2d0dde862a9e4e9ae8bb
ritesh0960 <riteshraushan0960@gmail.com>
2026-07-04 23:38:36 +0530

    push

--- src/main/java/com/aws/project/controller/AwsConfig.java
+++ src/main/java/com/aws/project/controller/AwsConfig.java
@@ -2,6 +2,8 @@ package com.aws.project.controller;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.ecr.AmazonECRClient;
+import com.amazonaws.services.ecr.AmazonECRClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
